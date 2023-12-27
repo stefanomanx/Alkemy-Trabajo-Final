c208e296d47e876715c37f11d00f6506ad9c9162
Stefano Mandolesi <stefano232@gmail.com>
2023-12-27 17:18:14 -0300

    arreglo de importacion de Navbar.css a Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { ShoppingCart } from 'phosphor-react';
-import './navbar.css'
+import './Navbar.css'
 
 // Componente de barra de navegación.
 
